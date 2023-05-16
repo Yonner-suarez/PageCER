3d1e74ab1c4e4abaa811c0df52a7a8a18df6f01d
Yonner-suarez <yonnerhazziel@gmail.com>
2023-05-16 17:19:16 -0500

    cambios

--- cer/src/components/RepuestosCitroen/RepuestosCitroen.jsx
+++ cer/src/components/RepuestosCitroen/RepuestosCitroen.jsx
@@ -1,9 +1,24 @@
 import "./RepuestosCitroen";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const RepuestosCitroen = () => {
   return (
-    <div>
-      <h1>AQUI REP Citroen</h1>
+    <div className="contenedorRepuestos">
+      <Card style={{ width: "20rem", margin: 5 }}>
+        <Card.Img
+          variant="top"
+          src="https://i.ebayimg.com/thumbs/images/g/b80AAOSw5P5gkmC5/s-l300.jpg"
+        />
+        <Card.Body>
+          <Card.Title>Kit mebrague</Card.Title>
+          <Card.Text>
+            Vw gol 1.6 1.8 02/... <br />
+            Precio: 120.000
+          </Card.Text>
+          <Button variant="primary">Comprar</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
--- cer/src/components/RepuestosVw/RepuestosVw.jsx
+++ cer/src/components/RepuestosVw/RepuestosVw.jsx
@@ -1,9 +1,38 @@
 import "./RepuestosVw.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const RepuestosVw = () => {
   return (
-    <div>
-      <h1>AQUI REP VW</h1>
+    <div className="contenedorRepuestos">
+      <Card style={{ width: "20rem", margin: 5 }}>
+        <Card.Img
+          variant="top"
+          src="https://i.ebayimg.com/thumbs/images/g/b80AAOSw5P5gkmC5/s-l300.jpg"
+        />
+        <Card.Body>
+          <Card.Title>Kit mebrague</Card.Title>
+          <Card.Text>
+            Vw gol 1.6 1.8 02/... <br />
+            Precio: 120.000
+          </Card.Text>
+          <Button variant="primary">Comprar</Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: "20rem", margin: 5 }}>
+        <Card.Img
+          variant="top"
+          src="https://i.ebayimg.com/thumbs/images/g/b80AAOSw5P5gkmC5/s-l300.jpg"
+        />
+        <Card.Body>
+          <Card.Title>Kit mebrague</Card.Title>
+          <Card.Text>
+            Vw gol 1.6 1.8 02/... <br />
+            Precio: 120.000
+          </Card.Text>
+          <Button variant="primary">Comprar</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
