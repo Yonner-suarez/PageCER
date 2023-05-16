4dc846423ca044d4bb6e15a13849b96957616787
Yonner-suarez <yonnerhazziel@gmail.com>
2023-05-16 10:16:37 -0500

    he aqui

--- cer/src/components/WhatsApp/WhatsApp.jsx
+++ cer/src/components/WhatsApp/WhatsApp.jsx
@@ -6,7 +6,10 @@ const WhatsAppInfo = () => {
   return (
     <div className="contenedorWhatsApp">
       <p className="parrafosWHS">Contactanos</p>
-      <Link to="https://www.whatsapp.com/?lang=es" className="link">
+      <Link
+        to="https://api.whatsapp.com/send?phone=573134421215&text=Buen_día_estoy_buscando_Repuestos"
+        className="link"
+      >
         <Whatsapp className="WhatsApp" />
       </Link>
     </div>
