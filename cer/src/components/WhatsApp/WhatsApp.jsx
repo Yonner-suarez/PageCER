e471514b70fde8f711c17bf0e33352af078a9851
Yonner-suarez <yonnerhazziel@gmail.com>
2023-05-16 10:09:45 -0500

    WHS

--- cer/src/components/WhatsApp/WhatsApp.jsx
+++ cer/src/components/WhatsApp/WhatsApp.jsx
@@ -1,11 +1,14 @@
 import "./WhatsApp.css";
 import { Whatsapp } from "react-bootstrap-icons";
+import { Link } from "react-router-dom";
 
 const WhatsAppInfo = () => {
   return (
     <div className="contenedorWhatsApp">
       <p className="parrafosWHS">Contactanos</p>
-      <Whatsapp className="WhatsApp" />
+      <Link to="https://www.whatsapp.com/?lang=es" className="link">
+        <Whatsapp className="WhatsApp" />
+      </Link>
     </div>
   );
 };
