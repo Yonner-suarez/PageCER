82dd2ba89e491138596d1ebcc6167056b0be3e29
Yonner-suarez <yonnerhazziel@gmail.com>
2023-05-16 09:10:33 -0500

    cambios

--- cer/src/App.jsx
+++ cer/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import RenderLogo from "./components/RenderLogo/RenderLogo";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/homePage/Home";
+import WhatsAppInfo from "./components/WhatsApp/WhatsApp";
 
 function App() {
   return (
     <div>
       <RenderLogo />
       <Home />
+      <WhatsAppInfo />
     </div>
   );
 }
---
+++ cer/src/components/WhatsApp/WhatsApp.jsx
@@ -0,0 +1,12 @@
+import "./WhatsApp.css";
+import { Whatsapp } from "react-bootstrap-icons";
+
+const WhatsAppInfo = () => {
+  return (
+    <div className="contenedorWhatsApp">
+      <p className="parrafosWHS">Contactanos</p>
+      <Whatsapp className="WhatsApp" />
+    </div>
+  );
+};
+export default WhatsAppInfo;
