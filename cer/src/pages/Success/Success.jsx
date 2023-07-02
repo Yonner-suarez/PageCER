6d9931e9697a2328bd12478adbb222ab746926b4
Yonner-suarezLilab <yonner.suarez@lilab.pe>
2023-07-02 16:50:14 -0500

    success page

--- cer/src/pages/Success/Success.jsx
+++ cer/src/pages/Success/Success.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import style from "./Success.module.css";
 import { useDispatch } from "react-redux";
 import { vaciarCarritoSucces } from "../../redux/slices/carrito";
+import { Asterisk } from "react-bootstrap-icons";
 
 const Success = () => {
   const dispatch = useDispatch();
@@ -10,7 +11,133 @@ const Success = () => {
     dispatch(vaciarCarritoSucces());
   }, [dispatch]);
 
-  return <div className={style.contenedorSuccess}>Success</div>;
+  return (
+    <div className={style.contenedorDosElementos}>
+      <div className={style.contenedorSuccess}>
+        <h1 className={style.envio}>Detalles del envio</h1>
+        <hr className={style.hr} />
+        <div className={style.contenedorNombres}>
+          <div className={style.formularioDeEvnio}>
+            {/* <div className={style.contenedorNombres}> */}
+            <input type="text" required className={style.input} />
+            <label className={style.label}>
+              <span className={style.span}>
+                Nombre{" "}
+                <Asterisk
+                  style={{ width: "6px", color: "red", marginTop: "-8px" }}
+                />
+              </span>
+            </label>
+          </div>
+          <div className={style.formularioDeEvnio}>
+            <input type="text" required className={style.input} />
+            <label className={style.label}>
+              <span className={style.span}>
+                Apellidos{" "}
+                <Asterisk
+                  style={{ width: "6px", color: "red", marginTop: "-8px" }}
+                />
+              </span>
+            </label>
+          </div>
+          <div className={style.formularioDeEvnio}>
+            <input type="text" required className={style.input} />
+            <label className={style.label}>
+              <span className={style.span}>
+                Pais{" "}
+                <Asterisk
+                  style={{ width: "6px", color: "red", marginTop: "-8px" }}
+                />
+              </span>
+            </label>
+          </div>
+          <div className={style.formularioDeEvnio}>
+            <input type="text" required className={style.input} />
+            <label className={style.label}>
+              <span className={style.span}>
+                Departamento{" "}
+                <Asterisk
+                  style={{ width: "6px", color: "red", marginTop: "-8px" }}
+                />
+              </span>
+            </label>
+          </div>
+          <div className={style.formularioDeEvnio}>
+            <input type="text" required className={style.input} />
+            <label className={style.label}>
+              <span className={style.span}>
+                Ciudad{" "}
+                <Asterisk
+                  style={{ width: "6px", color: "red", marginTop: "-8px" }}
+                />
+              </span>
+            </label>
+          </div>
+          <div className={style.formularioDeEvnio}>
+            <input type="text" required className={style.input} />
+            <label className={style.label}>
+              <span className={style.span}>
+                Dirección{" "}
+                <Asterisk
+                  style={{ width: "6px", color: "red", marginTop: "-8px" }}
+                />
+              </span>
+            </label>
+          </div>
+          <div className={style.formularioDeEvnio}>
+            <input type="text" required className={style.input} />
+            <label className={style.label}>
+              <span className={style.span}>
+                Información adicional dirección{" "}
+              </span>
+            </label>
+          </div>
+          <div className={style.formularioDeEvnio}>
+            <input type="text" required className={style.input} />
+            <label className={style.label}>
+              <span className={style.span}>
+                Código postal{" "}
+                <Asterisk
+                  style={{ width: "6px", color: "red", marginTop: "-8px" }}
+                />
+              </span>
+            </label>
+          </div>
+          <div className={style.formularioDeEvnio}>
+            <input type="text" required className={style.input} />
+            <label className={style.label}>
+              <span className={style.span}>
+                Telefono{" "}
+                <Asterisk
+                  style={{ width: "6px", color: "red", marginTop: "-8px" }}
+                />
+              </span>
+            </label>
+          </div>
+          <div className={style.formularioDeEvnio}>
+            <input type="text" required className={style.input} />
+            <label className={style.label}>
+              <span className={style.span}>
+                Correo electronico{" "}
+                <Asterisk
+                  style={{ width: "6px", color: "red", marginTop: "-8px" }}
+                />
+              </span>
+            </label>
+          </div>
+        </div>
+
+        {/* <h1 className={style.envio}>info adicional</h1>
+        <hr className={style.hr} />
+        <div className={style.formularioDeEvnio}>
+          <label>
+            <textarea type="text" />
+          </label>
+        </div> */}
+      </div>
+      <div className={style.imagenes}>imagen</div>
+    </div>
+  );
 };
 
 export default Success;
